7461d5559eb53b10bbc6e671659243a931a89111
linhahmed <45941034+linhahmed@users.noreply.github.com>
2019-11-22 20:20:21 +0200

    Add files via upload

--- src/eg/edu/alexu/csd/oop/db/DatabaseImp.java
+++ src/eg/edu/alexu/csd/oop/db/DatabaseImp.java
@@ -6,58 +6,58 @@ import java.io.File;
 
 public class DatabaseImp implements Database {
 
-    private static DatabaseImp Instance = null;
-    private ArrayList<File> database;
-    private SQL sql;
+	private static DatabaseImp Instance = null;
+	private ArrayList<File> database;
+	private SQL sql;
 
-    private DatabaseImp() {
-        this.database = new ArrayList<>();
-        this.sql = new SQL();
+	private DatabaseImp() {
+		this.database = new ArrayList<>();
+		this.sql = new SQL();
 
-    }
+	}
 
-    public static DatabaseImp getInstance() {
-        if (Instance == null) {
-            Instance = new DatabaseImp();
-        }
-        return Instance;
-    }
+	public static DatabaseImp getInstance() {
+		if (Instance == null) {
+			Instance = new DatabaseImp();
+		}
+		return Instance;
+	}
 
-    @Override
-    public String createDatabase(String databaseName, boolean dropIfExists) {
-        databaseName = databaseName.toLowerCase();
-        String query = "";
-        if (dropIfExists) {
-            query = "Drop Database " + databaseName;
-            try {
-                executeStructureQuery(query);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            query = "Create Database " + databaseName;
-            try {
-                executeStructureQuery(query);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        } else {
-            query = "Create Database " + databaseName;
-            try {
-                executeStructureQuery(query);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return sql.PathOfDatabase(databaseName);
-    }
+	@Override
+	public String createDatabase(String databaseName, boolean dropIfExists) {
+		databaseName = databaseName.toLowerCase();
+		String query = "";
+		if (dropIfExists) {
+			query = "Drop Database " + databaseName;
+			try {
+				executeStructureQuery(query);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			query = "Create Database " + databaseName;
+			try {
+				executeStructureQuery(query);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} else {
+			query = "Create Database " + databaseName;
+			try {
+				executeStructureQuery(query);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return sql.PathOfDatabase(databaseName);
+	}
 
-    @Override
-    public boolean executeStructureQuery(String query) throws SQLException {
-       query = query.toLowerCase();
+	@Override
+	public boolean executeStructureQuery(String query) throws SQLException {
+		query = query.toLowerCase();
 		boolean dropMatch = false;
 		boolean createMatch = false;
 		if (query.startsWith("drop")) {
-			dropMatch = parser.drop(query);
+			dropMatch = (boolean) parser.drop(query);
 		} else if (query.startsWith("create")) {
 			createMatch = parser.create(query);
 		}
@@ -67,7 +67,7 @@ public class DatabaseImp implements Database {
 			String[] splitted = query.replaceAll("\\)", " ").replaceAll("\\(", " ").replaceAll("'", "")
 					.replaceAll("\\s+\\,", ",").split("\\s+|\\,\\s*|\\(|\\)|\\=");
 			if (splitted[1].equalsIgnoreCase("database")) {
-                String databaseName = splitted[2].toLowerCase();
+				String databaseName = splitted[2].toLowerCase();
 				if (splitted[0].equalsIgnoreCase("create")) {
 					if (sql.databaseExists(databaseName)) {
 						int index = Index(databaseName);
@@ -87,7 +87,7 @@ public class DatabaseImp implements Database {
 					sql.dropDatabase(databaseName);
 				}
 			} else if (splitted[1].equalsIgnoreCase("table")) {
-                String TableName = splitted[2].toLowerCase();
+				String TableName = splitted[2].toLowerCase();
 				if (splitted[0].equalsIgnoreCase("create")) {
 
 				} else if (splitted[0].equalsIgnoreCase("drop")) {
@@ -96,32 +96,55 @@ public class DatabaseImp implements Database {
 			}
 		}
 		return true;
-    }
+	}
 
-    private int Index(String databaseName) {
-        int i = -1;
-        for (File flag : database) {
-            i++;
-            if (flag.getName().equalsIgnoreCase(databaseName)) {
-                return i;
-            }
+	private int Index(String databaseName) {
+		int i = -1;
+		for (File flag : database) {
+			i++;
+			if (flag.getName().equalsIgnoreCase(databaseName)) {
+				return i;
+			}
 
-        }
-        return -1;
-    }
+		}
+		return -1;
+	}
 
-    @Override
-    public Object[][] executeQuery(String query) throws SQLException {
+	@Override
+	public Object[][] executeQuery(String query) throws SQLException {
+		String [] splitted = query.replaceAll("\\)", " ").replaceAll("\\(", " ").replaceAll("\\s*'\\s*","'")
+				.replaceAll("\\s+\\,", ",").replaceAll("\\s*\"\\s*","\"").replaceAll("=", " = ")
+				.split("\\s+|\\(|\\)");
+		String columnName = splitted[1];
+		String tableName = splitted[3];
+		String DatabaseName = database.get(database.size()-1).getName();
+		
+		if(! sql.TableExists(DatabaseName, tableName)){
+			throw new RuntimeException("Table " + tableName +
+					" does not exists in " + DatabaseName);
+		}
+		
+		if(columnName.equals("*")){
+			
+		}
+		if(columnName.contains(",")){
+			String[] columnsName = columnName.split("\\s*,\\s*");
+		
+		}
+		// one column
+		else{
+		
+		}
 
-        return null;
-    }
+		return null;
+	}
 
-    @Override
-    public int executeUpdateQuery(String query) throws SQLException {
-        QueryValidation valid = new QueryValidation();
-        valid.updateValidation(query);
-        int rowsCount =0;
-        return rowsCount;
-    }
+	@Override
+	public int executeUpdateQuery(String query) throws SQLException {
+		QueryValidation valid = new QueryValidation();
+		valid.updateValidation(query);
+		int rowsCount = 0;
+		return rowsCount;
+	}
 
 }
