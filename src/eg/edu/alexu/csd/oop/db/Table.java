9e54f82074b3fb4b459c736b32897c845bca92cb
SalmaMarzouk <48380385+SalmaMarzouk@users.noreply.github.com>
2019-11-25 20:32:20 +0200

    Add files via upload

--- src/eg/edu/alexu/csd/oop/db/Table.java
+++ src/eg/edu/alexu/csd/oop/db/Table.java
@@ -24,26 +24,62 @@ public class Table {
         }
 
     }
+    public int delete (String condition, String operand , String value){
+        int counter = 0;
+        Document doc = DOMFactory.getDomObj(dataFile);
+        if(doc!=null){
+            NodeList rows = doc.getElementsByTagName("row");
+            if(operand.equals("=")){
+                for(int i=0;i<rows.getLength();i++){
+                    if(rows.item(i).getAttributes().getNamedItem(condition).getNodeValue().equals(value))
+                    {
+                        doc.getDocumentElement().removeChild(rows.item(i));
+                        counter++;
+                    }
+                }
+
+            }
+            if(operand.equals(">")) {
+                for (int i = 0; i < rows.getLength(); i++) {
+                    if (Integer.parseInt(rows.item(i).getAttributes().getNamedItem(condition).getNodeValue()) > Integer.parseInt(value)) {
+                        doc.getDocumentElement().removeChild(rows.item(i));
+                        counter++;
+                    }
+                }
+            }
+            if(operand.equals("<")){
+                for(int i=0;i<rows.getLength();i++){
+                    if(Integer.parseInt(rows.item(i).getAttributes().getNamedItem(condition).getNodeValue())<Integer.parseInt(value))
+                    {
+                        doc.getDocumentElement().removeChild(rows.item(i));
+                        counter++;
+                    }
+                }
+            }
+        }
+        return counter;
+    }
 
     public int insert(String[] columns , String [] values) {
 
-            Document doc = DOMFactory.getDomObj(dataFile);
-            if(doc!=null){
-                Element newRow = doc.createElement("row");
-                for (int i=0;i<columns.length;i++){
-                    newRow.setAttribute(columns[i],values[i]);
-                }
-                doc.getDocumentElement().appendChild(newRow);
-                if(DOMFactory.validateXML(doc,schemaFile)){
-                    DOMFactory.writeDOMtoFile(doc,dataFile);
-                }else {
-                    System.out.println("Not a valid query");
-                    return 0;
-                }
-                return columns.length;
+        Document doc = DOMFactory.getDomObj(dataFile);
+        if(doc!=null){
+            Element newRow = doc.createElement("row");
+            for (int i=0;i<columns.length;i++){
+                newRow.setAttribute(columns[i],values[i]);
             }
-            return 0;
+            doc.getDocumentElement().appendChild(newRow);
+            if(DOMFactory.validateXML(doc,schemaFile)){
+                DOMFactory.writeDOMtoFile(doc,dataFile);
+            }else {
+                System.out.println("Not a valid query");
+                return 0;
+            }
+            return columns.length;
+        }
+        return 0;
     }
+
     public int update (String[] columns , String [] values , String column, String value){
         int counter =0;
         Document doc = DOMFactory.getDomObj(dataFile);
@@ -65,7 +101,7 @@ public class Table {
             }
         }
         return counter;
-        }
+    }
 
     String getName(){
         return dataFile.getName().substring(0,dataFile.getName().length()-4);
