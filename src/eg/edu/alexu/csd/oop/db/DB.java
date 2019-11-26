1aab8f2a1686a4f0af3a6e9bdb3f1c501cec4785
linhahmed <45941034+linhahmed@users.noreply.github.com>
2019-11-26 05:51:41 +0200

    Add files via upload

--- src/eg/edu/alexu/csd/oop/db/DB.java
+++ src/eg/edu/alexu/csd/oop/db/DB.java
@@ -4,52 +4,68 @@ import java.io.File;
 import java.util.ArrayList;
 
 public class DB {
-    private File directory;
-    private ArrayList<Table> tables;
+	private File directory;
+	private ArrayList<Table> tables;
 
-    public DB(String name, File parentDirectory) {
-        tables = new ArrayList<>();
-        directory = new File(parentDirectory, name);
-        if (!directory.mkdir()) {
-            File[] oldTables = directory.listFiles();
-            if (oldTables != null) {
-                for (File table : oldTables
-                ) {
-                    if (table.isFile() && table.getName().endsWith(".xml")) {
-                        tables.add(new Table(table.getName().substring(0, table.getName().length() - 4), directory,null));
-                    }
-                }
-            }
-        }
-    }
-    public String getName(){
-        return directory.getName();
-    }
-    public boolean deleteDatabase(){
-        return directory.delete();
-    }
+	public DB(String name, File parentDirectory) {
+		tables = new ArrayList<>();
+		directory = new File(parentDirectory, name);
+		if (!directory.mkdir()) {
+			File[] oldTables = directory.listFiles();
+			if (oldTables != null) {
+				for (File table : oldTables) {
+					if (table.isFile() && table.getName().endsWith(".xml")) {
+						tables.add(
+								new Table(table.getName().substring(0, table.getName().length() - 4), directory, null));
+					}
+				}
+			}
+		}
+	}
 
-    public boolean addTable(String name,String[][] columns){
-        if(tableExist(name)){
-            return false;
-        }else {
-            tables.add(new Table(name,directory,columns));
-            return true;
-        }
+	public String getName() {
+		return directory.getName();
+	}
 
-    }
-    private boolean tableExist(String name){
-        for (Table t:tables
-             ) {
-            if(name.equalsIgnoreCase(t.getName())){
-                return true;
-            }
-        }
-        return false;
-    }
+	public boolean deleteDatabase() {
+		return directory.delete();
+	}
 
+	public boolean addTable(String name, String[][] columns) {
+		if (tableExist(name)) {
+			return false;
+		} else {
+			tables.add(new Table(name, directory, columns));
+			return true;
+		}
 
-    public boolean deleteTable(String test, String[] strings) {
-        return true;
-    }
+	}
+
+	public boolean tableExist(String name) {
+		for (Table t : tables) {
+			if (name.equalsIgnoreCase(t.getName())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public boolean deleteTable(String test, String[] strings) {
+		return true;
+	}
+
+	public int getTableIndex(String name) {
+		int i = 0;
+		for (Table table : tables) {
+			if (table.getName().equalsIgnoreCase(name))
+				return i;
+			i++;
+		}
+		return -1;
+	}
+
+	public ArrayList<Table> getTables() {
+		return tables;
+	}
+
 }
