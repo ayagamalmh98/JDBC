697be45c3d4ac234c2fbd073a0ab52356f09b682
linhahmed <45941034+linhahmed@users.noreply.github.com>
2019-11-29 20:14:20 +0200

    Add files via upload

--- src/eg/edu/alexu/csd/oop/db/UI.java
+++ src/eg/edu/alexu/csd/oop/db/UI.java
@@ -6,18 +6,22 @@ import java.util.Scanner;
 import eg.edu.alexu.csd.oop.db.DBMSController;
 
 public class UI {
-	
+
 	private static Scanner sc;
-	public static void main(String [] args) {
+
+	public static void main(String[] args) {
 		sc = new Scanner(System.in);
 		DBMSController executor = DBMSController.getInstance();
-		while(true) {
+		while (true) {
 			System.out.println("Enter your Input :");
 			String query = sc.nextLine();
+			if (query.isEmpty()) {
+				query = sc.nextLine();
+	        }
 			try {
 				System.out.println(executor.invoke(query));
 			} catch (SQLException e) {
-				System.out.println("Error!");
+				e.printStackTrace();
 			}
 		}
 	}
