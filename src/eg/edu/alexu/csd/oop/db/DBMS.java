42201075bbaf925d7fe2bda9a173f853ea582b2d
linhahmed <45941034+linhahmed@users.noreply.github.com>
2019-11-29 21:15:53 +0200

    Add files via upload

--- src/eg/edu/alexu/csd/oop/db/DBMS.java
+++ src/eg/edu/alexu/csd/oop/db/DBMS.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 import eg.edu.alexu.csd.oop.db.parser.Parser;
+import eg.edu.alexu.csd.oop.db.parser.QV;
 import eg.edu.alexu.csd.oop.db.parser.ValidReadQuery;
 import eg.edu.alexu.csd.oop.db.parser.ValidStructure;
 import eg.edu.alexu.csd.oop.db.parser.ValidUpdateQuery;
@@ -14,16 +15,12 @@ public class DBMS implements Database {
 
     private File workspace;
     private ArrayList<DB> databases;
-    private Parser ValidReadQuery;
-    private Parser ValidStructure;
-    private Parser ValidUpdateQuery;
+    private QV Validator;
 
     public DBMS()  {
         databases = new ArrayList<>();
         workspace = new File("workspace");
-        ValidReadQuery = new ValidReadQuery();
-        ValidStructure = new ValidStructure();
-        ValidUpdateQuery =new ValidUpdateQuery();
+        Validator = new QV();
         if (!workspace.mkdir()) {
             deleteFile(workspace);
             workspace = new File("workspace");
@@ -68,7 +65,7 @@ public class DBMS implements Database {
 
     public boolean executeStructureQuery(String query) throws java.sql.SQLException {
         query= query.toLowerCase();
-        if (!ValidStructure.isValid(query)) {
+        if (!Validator.ValidStructure(query)) {
             throw new SQLException("Invalid query");
         }
         String LQuery = query.toLowerCase();
@@ -111,7 +108,7 @@ public class DBMS implements Database {
         query= query.toLowerCase();
         DataCarrier carrier;
         String LQuery = query.toLowerCase();
-        if (!ValidReadQuery.isValid(query)) {
+        if (!Validator.ValidReadQuery(query)) {
             throw new SQLException("Invalid query");
         }
         if (databases.isEmpty()) {
@@ -163,7 +160,7 @@ public class DBMS implements Database {
 
     public int executeUpdateQuery(String query) throws java.sql.SQLException {
         query= query.toLowerCase();
-        if (!ValidUpdateQuery.isValid(query)) {
+        if (!Validator.ValidUpdateQuery(query)) {
             throw new SQLException("Invalid query");
         }
         DataCarrier carrier;
