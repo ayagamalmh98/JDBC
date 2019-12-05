f5604ef195ad4213329a5c0e8cebf9a2db23c1d7
SalmaMarzouk <48380385+SalmaMarzouk@users.noreply.github.com>
2019-12-05 16:22:47 +0200

    Add files via upload

--- src/eg/edu/alexu/csd/oop/jdbc/ResultSetMetaDataImp.java
+++ src/eg/edu/alexu/csd/oop/jdbc/ResultSetMetaDataImp.java
@@ -2,127 +2,132 @@ package eg.edu.alexu.csd.oop.jdbc;
 
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
+public class ResultSetMetaDataImp implements ResultSetMetaData {
+    private String [][] columnsInfo;
+    private Object [][] productedData;
+    private String tableName;
+    public ResultSetMetaDataImp ( Object [][] productedData, String [][] columnsInfo, String tableName  ) {
+        this.productedData=productedData;
+        this.columnsInfo=columnsInfo;
+        this.tableName=tableName;
+    }
 
-public class ResultSetMetaDataImp implements ResultSetMetaData{
+    @Override
+    public int getColumnCount() throws SQLException {
+        return  productedData[0].length;
+    }
 
-	@Override
-	public boolean isWrapperFor(Class<?> iface) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean isAutoIncrement(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public <T> T unwrap(Class<T> iface) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean isCaseSensitive(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public String getCatalogName(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean isSearchable(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public String getColumnClassName(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean isCurrency(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public int getColumnCount() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int isNullable(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public int getColumnDisplaySize(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean isSigned(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public String getColumnLabel(int arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public int getColumnDisplaySize(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public String getColumnName(int arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getColumnLabel(int column) throws SQLException {
+        return null;
+    }
 
-	@Override
-	public int getColumnType(int arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public String getColumnName(int column) throws SQLException {
+        return columnsInfo[0][column];
+    }
 
-	@Override
-	public String getColumnTypeName(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public String getSchemaName(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public int getPrecision(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public int getPrecision(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public int getScale(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public int getScale(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public String getSchemaName(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public String getTableName(int column) throws SQLException {
+        return tableName;
+    }
 
-	@Override
-	public String getTableName(int arg0) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getCatalogName(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public boolean isAutoIncrement(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public int getColumnType(int column) throws SQLException {
+       if(columnsInfo[1][column].equalsIgnoreCase("string"))
+           return 12;
+       else
+           return 4;
+    }
 
-	@Override
-	public boolean isCaseSensitive(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public String getColumnTypeName(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public boolean isCurrency(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean isReadOnly(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public boolean isDefinitelyWritable(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean isWritable(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public int isNullable(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public boolean isDefinitelyWritable(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public boolean isReadOnly(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public String getColumnClassName(int column) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public boolean isSearchable(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 
-	@Override
-	public boolean isSigned(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public boolean isWritable(int arg0) throws SQLException {
-		throw new UnsupportedOperationException();
-	}
-
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        throw new UnsupportedOperationException();
+    }
 }
