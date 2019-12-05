61ea3f681cd53a7ab68af83b9c8c8217ef1f44b7
linhahmed <45941034+linhahmed@users.noreply.github.com>
2019-12-05 16:02:48 +0200

    Add files via upload

--- DBMS/src/eg/edu/alexu/csd/oop/db/DBMSController.java
+++ DBMS/src/eg/edu/alexu/csd/oop/db/DBMSController.java
@@ -5,83 +5,140 @@ import eg.edu.alexu.csd.oop.db.parser.QV;
 import java.sql.SQLException;
 
 public class DBMSController {
-    private DBMSController() throws SQLException {
-        manager = new DBMS();
-    }
-
-    private static DBMSController instance;
-    private static Database manager;
-    private static QV validator;
-    private static DataCarrier info;
-
-    static {
-        try {
-            instance = new DBMSController();
-            validator = new QV();
-        } catch (SQLException s) {
-
-        }
-    }
-
-    public static DBMSController getInstance() {
-        return instance;
-    }
-
-    public String invoke(String query) throws SQLException {
-        query = query.toLowerCase();
-        int operation = validator.isValidQuery(query);
-        if (operation == 1 || operation == 2) {
-            boolean test = manager.executeStructureQuery(query);
-            if (test) {
-                return ((operation == 1 ? "Database" : "Table") + " created successfully.");
-            } else {
-                return ((operation == 1 ? "Database" : "Table") + " wasn't created successfully.");
-
-            }
-        } else if (operation == 3 || operation == 4) {
-            boolean test1 = manager.executeStructureQuery(query);
-            if (test1) {
-                return ((operation == 3 ? "Database" : "Table") + " dropped successfully.");
-            } else {
-                return ((operation == 3 ? "Database" : "Table") + " wasn't dropped successfully.");
-            }
-        } else if (operation >= 5 && operation <= 8) {
-            Object[][] test2 = manager.executeQuery(query);
-            if (test2 == null) {
-                return "Wrong selection!!";
-            } else {
-                StringBuilder st = new StringBuilder();
-                for (Object[] objects : test2) {
-                    for (int j = 0; j < test2[0].length; j++) {
-                        st.append(objects[j].toString()).append(" ");
-                    }
-                    st.append("\n");
-                }
-                return st.toString();
-            }
-        } else if (operation >= 9 && operation <= 14) {
-                return manager.executeUpdateQuery(query) + " rows has been Updated.";
-
-        } else {
-            throw new SQLException("Not a valid SQL query!");
-        }
-    }
-    
-    public String[][] getColumnsInfo() {
-    	String[] names = info.columns;
-    	String[] types = info.columnsTypes;
-    	String[][] op = new String[2][names.length];
-    	for( int i =0;i<op.length;i++) {
-    		op[0][i]=names[i];
-    		op[1][i]=types[i];
-    	}
-		return op;
-	}
-    
-    public String getTableName() {
-		return info.tableName;
+	private DBMSController() throws SQLException {
+		manager = new DBMS();
 	}
 
+	private static DBMSController instance;
+	private static Database manager;
+	private static QV validator;
 
+	static {
+		try {
+			instance = new DBMSController();
+			validator = new QV();
+		} catch (SQLException s) {
+
+		}
+	}
+
+	public static DBMSController getInstance() {
+		return instance;
+	}
+
+	public String invoke(String query) throws SQLException {
+		query = query.toLowerCase();
+		int operation = validator.isValidQuery(query);
+		if (operation == 1 || operation == 2) {
+			boolean test = manager.executeStructureQuery(query);
+			if (test) {
+				return ((operation == 1 ? "Database" : "Table") + " created successfully.");
+			} else {
+				return ((operation == 1 ? "Database" : "Table") + " wasn't created successfully.");
+
+			}
+		} else if (operation == 3 || operation == 4) {
+			boolean test1 = manager.executeStructureQuery(query);
+			if (test1) {
+				return ((operation == 3 ? "Database" : "Table") + " dropped successfully.");
+			} else {
+				return ((operation == 3 ? "Database" : "Table") + " wasn't dropped successfully.");
+			}
+		} else if (operation >= 5 && operation <= 8) {
+			Object[][] test2 = manager.executeQuery(query);
+			if (test2 == null) {
+				return "Wrong selection!!";
+			} else {
+				StringBuilder st = new StringBuilder();
+				for (Object[] objects : test2) {
+					for (int j = 0; j < test2[0].length; j++) {
+						st.append(objects[j].toString()).append(" ");
+					}
+					st.append("\n");
+				}
+				return st.toString();
+			}
+		} else if (operation >= 9 && operation <= 14) {
+			return manager.executeUpdateQuery(query) + " rows has been Updated.";
+
+		} else {
+			throw new SQLException("Not a valid SQL query!");
+		}
+	}
+
+	public String[][] getColumnsInfo(String query) throws SQLException {
+		query = query.toLowerCase();
+		DataCarrier carrier;
+		int operation = validator.validReadQuery(query);
+		if (operation < 0) {
+			throw new SQLException("Invalid query");
+		}
+		if (operation == 5) {
+			carrier = DataExtractor.getInstance().selectAllData(query);
+			String[] names = carrier.columns;
+			String[] types = carrier.columnsTypes;
+			String[][] op = new String[2][names.length];
+			for (int i = 0; i < op.length; i++) {
+				op[0][i] = names[i];
+				op[1][i] = types[i];
+			}
+			return op;
+		} else if (operation == 6) {
+			carrier = DataExtractor.getInstance().selectAllWhereData(query);
+			String[] names = carrier.columns;
+			String[] types = carrier.columnsTypes;
+			String[][] op = new String[2][names.length];
+			for (int i = 0; i < op.length; i++) {
+				op[0][i] = names[i];
+				op[1][i] = types[i];
+			}
+			return op;
+		} else if (operation == 7) {
+			carrier = DataExtractor.getInstance().selectSomeData(query);
+			String[] names = carrier.columns;
+			String[] types = carrier.columnsTypes;
+			String[][] op = new String[2][names.length];
+			for (int i = 0; i < op.length; i++) {
+				op[0][i] = names[i];
+				op[1][i] = types[i];
+			}
+			return op;
+
+		} else if (operation == 8) {
+			carrier = DataExtractor.getInstance().selectSomeWhereData(query);
+			String[] names = carrier.columns;
+			String[] types = carrier.columnsTypes;
+			String[][] op = new String[2][names.length];
+			for (int i = 0; i < op.length; i++) {
+				op[0][i] = names[i];
+				op[1][i] = types[i];
+			}
+			return op;
+		}
+		return null;
+	}
+
+	public String getTableName(String query) throws SQLException {
+		query = query.toLowerCase();
+		DataCarrier carrier;
+		int operation = validator.validReadQuery(query);
+		if (operation < 0) {
+			throw new SQLException("Invalid query");
+		}
+		if (operation == 5) {
+			carrier = DataExtractor.getInstance().selectAllData(query);
+			return carrier.tableName;
+		} else if (operation == 6) {
+			carrier = DataExtractor.getInstance().selectAllWhereData(query);
+			return carrier.tableName;
+		} else if (operation == 7) {
+			carrier = DataExtractor.getInstance().selectSomeData(query);
+			return carrier.tableName;
+		} else if (operation == 8) {
+			carrier = DataExtractor.getInstance().selectSomeWhereData(query);
+			return carrier.tableName;
+		}
+		return null;
+	}
 
 }
