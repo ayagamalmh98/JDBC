fe1c5e623aa0f630a94b40e7ae348bbb0838a6d5
SalmaMarzouk <48380385+SalmaMarzouk@users.noreply.github.com>
2019-12-06 15:41:32 +0200

    Add files via upload

--- DBMS/src/eg/edu/alexu/csd/oop/db/parser/ValidReadQuery.java
+++ DBMS/src/eg/edu/alexu/csd/oop/db/parser/ValidReadQuery.java
@@ -8,13 +8,14 @@ public class ValidReadQuery implements Parser {
     private static final String selectAllWherePattern = "(\\A)(?i)(\\s*)(select)(\\s*)[*](\\s*)(from)(\\s+)(\\w+)(\\s+)(where)(\\s+)(\\w+)(\\s*)([=<>])(\\s*)(([']([^'])*['])|([0-9]+))(\\s*)(?-i)(\\z)";
     private static final String selectSomePattern = "(\\A)(?i)(\\s*)(select)(\\s+)(((\\s*)(\\w+)(\\s*)[,](\\s*))*((\\s*)(\\w+)(\\s*)))(\\s+)(from)(\\s+)(\\w+)(\\s*)(?-i)(\\z)";
     private static final String selectSomeWherePattern = "(\\A)(?i)(\\s*)(select)(\\s+)(((\\s*)(\\w+)(\\s*)[,](\\s*))*((\\s*)(\\w+)(\\s*)))(\\s+)(from)(\\s+)(\\w+)(\\s+)(where)(\\s+)(\\w+)(\\s*)([=<>])(\\s*)(([']([^'])*['])|([0-9]+))(\\s*)(?-i)(\\z)";
-
+    private static final String selectAsPattern = "(\\A)(?i)(\\s*)(select)(\\s+)((((\\s*)(\\w+)(\\s+)(as)(\\s+)(\\w+)(\\s*)[,](\\s*))*(\\s*)(\\w+)(\\s+)(as)(\\s+)(\\w+)(\\s+)))(from)(\\s+)(\\w+)(\\s*)(?-i)([;])?(\\s*)";
     public int isValid(String query) {
         return selectIsValid(query);
     }
 
-    private int selectIsValid(String query) {
-        return Math.max(Math.max(regexMatcher(query, selectAllPattern) ? 5 : -1, regexMatcher(query, selectAllWherePattern) ? 6 : -1), Math.max(regexMatcher(query, selectSomePattern) ? 7 : -1, regexMatcher(query, selectSomeWherePattern) ? 8 : -1));
+
+      private int selectIsValid(String query) {
+        return Math.max(Math.max(regexMatcher(query, selectAllPattern) ? 5 : -1, regexMatcher(query, selectAllWherePattern) ? 6 : -1),Math.max(Math.max(regexMatcher(query, selectSomePattern) ? 7 : -1, regexMatcher(query, selectSomeWherePattern) ? 8 : -1),regexMatcher(query,selectAsPattern)?15:-1));
     }
 
     private boolean regexMatcher(String input, String pattern) {
