712d6a71a87c9208dda948e6d86cbe0f7a50bf90
linhahmed <45941034+linhahmed@users.noreply.github.com>
2019-12-06 12:14:32 +0200

    Add files via upload

---
+++ src/eg/edu/alexu/csd/oop/db/DataCarrier.java
@@ -0,0 +1,16 @@
+package eg.edu.alexu.csd.oop.db;
+
+ public class DataCarrier {
+     DataCarrier() {
+    }
+
+     String DBName;
+     String tableName;
+     String conditionColumn;
+     String conditionValue;
+     char conditionOperator;
+     public String[] columns;
+     public String[] columnsTypes;
+     String[] values;
+
+}
