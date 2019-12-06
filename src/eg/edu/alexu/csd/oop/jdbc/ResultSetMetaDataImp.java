7cf99a49bc21e759d2fc6c28812017d27c4060f1
linhahmed <45941034+linhahmed@users.noreply.github.com>
2019-12-06 14:09:05 +0200

    Add files via upload

--- src/eg/edu/alexu/csd/oop/jdbc/ResultSetMetaDataImp.java
+++ src/eg/edu/alexu/csd/oop/jdbc/ResultSetMetaDataImp.java
@@ -5,135 +5,142 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import eg.edu.alexu.csd.oop.db.DataCarrier;
+
 public class ResultSetMetaDataImp implements ResultSetMetaData {
-    private DataCarrier columnsInfo;
-    private Object [][] productedData;
-    private String tableName;
-    public ResultSetMetaDataImp ( Object [][] productedData, DataCarrier columnsInfo, String tableName  ) {
-        this.productedData=productedData;
-        this.columnsInfo=columnsInfo;
-        this.tableName=tableName;
-    }
+	private DataCarrier columnsInfo;
+	private Object[][] productedData;
+	private String tableName;
 
-    @Override
-    public int getColumnCount() throws SQLException {
-        return  productedData[0].length;
-    }
+	public ResultSetMetaDataImp(Object[][] productedData, DataCarrier columnsInfo, String tableName) {
+		this.productedData = productedData;
+		this.columnsInfo = columnsInfo;
+		this.tableName = tableName;
+	}
 
-    @Override
-    public boolean isAutoIncrement(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public int getColumnCount() throws SQLException {
+		if(productedData.length == 0) {
+			return 0;	
+		}else {
+			return productedData[0].length;
+		}
 
-    @Override
-    public boolean isCaseSensitive(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	}
 
-    @Override
-    public boolean isSearchable(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean isAutoIncrement(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean isCurrency(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean isCaseSensitive(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public int isNullable(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean isSearchable(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean isSigned(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean isCurrency(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public int getColumnDisplaySize(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public int isNullable(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public String getColumnLabel(int column) throws SQLException {
-        return null;
-    }
+	@Override
+	public boolean isSigned(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public String getColumnName(int column) throws SQLException {
-    	String[] names = columnsInfo.columns;
-        return names[column];
-    }
+	@Override
+	public int getColumnDisplaySize(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public String getSchemaName(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public String getColumnLabel(int column) throws SQLException {
+		return null;
+	}
 
-    @Override
-    public int getPrecision(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public String getColumnName(int column) throws SQLException {
+		String[] names = columnsInfo.columns;
+		return names[column];
+	}
 
-    @Override
-    public int getScale(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public String getSchemaName(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public String getTableName(int column) throws SQLException {
-        return tableName;
-    }
+	@Override
+	public int getPrecision(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public String getCatalogName(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public int getScale(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public int getColumnType(int column) throws SQLException {
-    	String[] types = columnsInfo.columnsTypes;
-       if(types[column].equalsIgnoreCase("string"))
-           return 12;
-       else if(types[column].equalsIgnoreCase("int"))
-           return 4;
-       else
-    	   return 0;
-    }
+	@Override
+	public String getTableName(int column) throws SQLException {
+		return tableName;
+	}
 
-    @Override
-    public String getColumnTypeName(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public String getCatalogName(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean isReadOnly(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public int getColumnType(int column) throws SQLException {
+		String[] types = columnsInfo.columnsTypes;
+		if (types[column].equalsIgnoreCase("string"))
+			return 12;
+		else if (types[column].equalsIgnoreCase("int"))
+			return 4;
+		else
+			return 0;
+	}
 
-    @Override
-    public boolean isWritable(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public String getColumnTypeName(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean isDefinitelyWritable(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean isReadOnly(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public String getColumnClassName(int column) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean isWritable(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public <T> T unwrap(Class<T> iface) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public boolean isDefinitelyWritable(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 
-    @Override
-    public boolean isWrapperFor(Class<?> iface) throws SQLException {
-        throw new UnsupportedOperationException();
-    }
+	@Override
+	public String getColumnClassName(int column) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public <T> T unwrap(Class<T> iface) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean isWrapperFor(Class<?> iface) throws SQLException {
+		throw new UnsupportedOperationException();
+	}
 }
--- src/eg/edu/alexu/csd/oop/jdbc/StatementImp.java
+++ src/eg/edu/alexu/csd/oop/jdbc/StatementImp.java
@@ -11,25 +11,54 @@ import eg.edu.alexu.csd.oop.db.DBMSController;
 import eg.edu.alexu.csd.oop.db.DataCarrier;
 
 public class StatementImp implements Statement {
+
+	private logger logger;
 	private ConnectionImp connection;
 	private ResultsetImp resultSet;
 	private ArrayList<String> batches;
 	private Object[][] ProducedData;
 	private int time = 0;
+	private boolean isClose;
+	DBMSController executor;
 
 	public StatementImp(ConnectionImp con) {
+		logger = logger.getInstance();
+		executor = DBMSController.getInstance();
+		logger.log.info("Building Statement object.");
 		this.connection = con;
+		isClose = false;
 	}
 
 	@Override
 	public void addBatch(String arg0) throws SQLException {
-		batches.add(arg0);
+		try {
+			if (isClose) {
+
+				throw new SQLException("Statement is closed ..Can't do operations");
+			}
+			logger.log.info("adding new Batch.");
+			batches.add(arg0);
+
+		} catch (Exception e) {
+			logger.log.info("Error in adding Batch");
+
+		}
 
 	}
 
 	@Override
 	public void clearBatch() throws SQLException {
-		batches.clear();
+		try {
+			if (isClose) {
+				throw new SQLException("Statement is closed ..Can't do operations");
+			}
+			logger.log.info("Empties list of SQL commands.");
+			batches.clear();
+
+		} catch (Exception e) {
+			logger.log.info("Error in clearing Batch List");
+
+		}
 
 	}
 
@@ -38,54 +67,82 @@ public class StatementImp implements Statement {
 		connection = null;
 		batches = null;
 		resultSet = null;
+		isClose = true;
 	}
 
 	@Override
 	public boolean execute(String arg0) throws SQLException {
-		DBMSController executor = DBMSController.getInstance();
-		String s = executor.invoke(arg0);
-		if (executeQuery(arg0).next()) {
-			return true;
+		if (isClose) {
+			logger.log.warning("Statement is closed.");
+			throw new SQLException("Statement is closed ..Can't do operations");
 		}
-		if (s.contains("dropped"))
-			return true; // according to tests :"|
+		logger.log.info("Executing the given SQL statement");
 
+		if (arg0.trim().split("\\s+")[0].equalsIgnoreCase("create")
+				|| arg0.trim().split("\\s+")[0].equalsIgnoreCase("drop")) {
+			String s = executor.invoke(arg0);
+			if(s.contains("wasn't")) {
+				return false;
+			} else {
+				return true;
+			}
+		} else if (arg0.trim().split("\\s+")[0].equalsIgnoreCase("insert")
+				|| arg0.trim().split("\\s+")[0].equalsIgnoreCase("delete")
+				|| arg0.trim().split("\\s+")[0].equalsIgnoreCase("update")) {
+			int result = executeUpdate(arg0);
+			return result > 0;
+		} else if (arg0.trim().split("\\s+")[0].equalsIgnoreCase("select")) {
+			logger.log.info("Generating result of select query..");
+			ResultSet result = executeQuery(arg0);
+			int k = result.getMetaData().getColumnCount();
+			return result.getMetaData().getColumnCount() > 0;
+		}
 		return false;
 	}
 
+	
 	@Override
 	public ResultSet executeQuery(String arg0) throws SQLException {
-		DBMSController executor = DBMSController.getInstance();
-		String s = executor.invoke(arg0);
+		if (isClose) {
+			logger.log.warning("Statement is closed.");
+			throw new SQLException("Statement is closed ..Can't do operations");
+		}
+		logger.log.info("Executing the given SQL statement and returning ResultSet ");
+		Object[][] ProducedData = executor.invokee(arg0);
 		String tablename = executor.getTableName(arg0);
 		DataCarrier columnsinfo = executor.getColumnsInfo(arg0);
-		Object[] ss = s.split("\\n");
-		ProducedData = new String[ss.length][((String) ss[0]).length()];
-		for (int i = 0; i < ss.length; i++) {
-			Object[] t =  ((String) ss[i]).split("\\s");
-			ProducedData[i] = t;
-		}
 		resultSet = new ResultsetImp(ProducedData, columnsinfo, tablename, this);
 		return resultSet;
 	}
 
 	@Override
 	public int executeUpdate(String arg0) throws SQLException {
-		DBMSController executor = DBMSController.getInstance();
+		if (isClose) {
+			logger.log.warning("Statement is closed.");
+			throw new SQLException("Statement is closed ..Can't do operations");
+		}
+		logger.log.info("Executing the given SQL Update statement.");
+
 		String s = executor.invoke(arg0);
 		String[] ss = s.split("\\s");
 		return Integer.parseInt(ss[0]);
+
 	}
 
 	@Override
 	public int[] executeBatch() throws SQLException {
+		if (isClose) {
+			logger.log.warning("Statement is closed.");
+			throw new SQLException("Statement is closed ..Can't do operations");
+		}
+		logger.log.info("Executing list of SQL commands.");
 		int[] arr = new int[batches.size()];
 		for (int i = 0; i < batches.size(); i++) {
 			String query = batches.get(i);
-			DBMSController executor = DBMSController.getInstance();
 			String s = executor.invoke(query);
 			if (execute(query))
 				arr[i] = SUCCESS_NO_INFO;
+
 			else
 				arr[i] = executeUpdate(query);
 		}
@@ -94,23 +151,43 @@ public class StatementImp implements Statement {
 
 	@Override
 	public int getQueryTimeout() throws SQLException {
+		if (isClose) {
+			logger.log.warning("Statement is closed.");
+			throw new SQLException("Statement is closed ..Can't do operations");
+		}
+		logger.log.info("getting the current query timeout limit ");
 
 		return time;
 	}
 
 	@Override
 	public void setQueryTimeout(int arg0) throws SQLException {
+		if (isClose) {
+			logger.log.warning("Statement is closed.");
+			throw new SQLException("Statement is closed ..Can't do operations");
+		}
+		logger.log.info("setting the current query timeout limit ");
 		time = arg0;
 
 	}
 
 	@Override
 	public Connection getConnection() throws SQLException {
+		if (isClose) {
+			logger.log.warning("Statement is closed.");
+			throw new SQLException("Statement is closed ..Can't do operations");
+		}
+		logger.log.info("Getting the connection that produced this statement");
 		return connection;
 	}
 
 	@Override
 	public ResultSet getResultSet() throws SQLException {
+		if (isClose) {
+			logger.log.warning("Statement is closed.");
+			throw new SQLException("Statement is closed ..Can't do operations");
+		}
+		logger.log.info("Getting the current result as a ResultSet object ");
 		return this.resultSet;
 	}
 
